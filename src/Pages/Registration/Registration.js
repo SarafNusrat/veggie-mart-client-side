3bcc5db6621d0baba3fc7287bbc045249d3a64e2
SarafNusrat <saraf.nusrat11@gmail.com>
2022-05-13 21:01:55 +0600

    extra 2 section added

---
+++ src/Pages/Registration/Registration.js
@@ -0,0 +1,11 @@
+import React from 'react';
+
+const Registration = () => {
+    return (
+        <div>
+            <h2>Ths is registration</h2>
+        </div>
+    );
+};
+
+export default Registration;
--- tailwind.config.js
+++ tailwind.config.js
@@ -1,10 +1,11 @@
 module.exports = {
   content: [
     "./src/**/*.{js,jsx,ts,tsx}",
-    "./node_modules/flowbite/**/*.js"
+    "./node_modules/flowbite/**/*.js",
+    './node_modules/tw-elements/dist/js/**/*.js'
   ],
   theme: {
     extend: {},
   },
-  plugins: [require("daisyui",'flowbite/plugin')],
+  plugins: [require("daisyui",'flowbite/plugin','tw-elements/dist/plugin')],
 }
